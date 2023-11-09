d05dfd31b0cca9d6caea17da72e4c52e161adca9
Hyeonseok0627 <stonehead0627@gmail.com>
2023-11-09 14:14:54 +0900

    231109 5교시 함수형 컴포넌트 hooks 2 useReducer 테스트 준비

--- chjoin-test/src/ch8_hooksTest/InfoTestUseEffect.js
+++ chjoin-test/src/ch8_hooksTest/InfoTestUseEffect.js
@@ -1,6 +1,7 @@
 //훅스 useState 먼저 확인.
 // 복습 해보기.
 // 지금부터는 모두 함수형 컴포넌트로 작업하기.
+import { Button } from "antd";
 import React, { useState, useEffect } from "react";
 
 const InfoTestUseEffect = () => {
@@ -12,18 +13,26 @@ const InfoTestUseEffect = () => {
   const [nickname, setNickname] = useState("");
 
   // useEffect Test 하기.
-  // 정의 useEffect(콜백함수, 의존성 배열)
-  // 의존성 배열 모양
-  // 1) 아무것도 없을 때, 매번 콜백함수 실행되고,
-  // 2) [], 빈 배열, 한번만 실행되고
-  // 3) [list], list의 상태가 변경 될 때마다, 콜백함수가 실행이 됨.
+  //정의 useEffect(콜백함수,의존성 배열)
+  // 의존성 배열 모양 1) 아무것도 없을 때, 매번 콜백함수 실행되고,
+  // 2) [] , 빈 배열, 한번만 실행되고
+  // 3) [list] , list의 상태가 변경 될 때 마다, 콜백함수 실행이 됨.
   useEffect(() => {
-    console.log("useEffect 호출이 됨. ");
+    console.log("useEffect 호출이됨. ");
     console.log({
       name,
       nickname,
     });
-  }); // 현재, 두번째 매개변수에 모양이 아무것도 없다. 매번 실행 확인하기.
+    // 후처리 함수 추가하기.
+    // 문법 : return () => {수행할 로직}
+    return () => {
+      console.log("후처리 함수 호출 ");
+      console.log(name);
+    };
+  }, [name]); // 현재, 두번째 매개변수에 모양이 아무것도 없다. 매번 실행 확인 하기.
+
+  //추가, 버튼 추가해서, visible 속성 확인.
+  const [visible, setVisible] = useState(false);
 
   // 이벤트 핸들러 추가
   // 1. 이름 캐멀케이스 표기법, 2. 인자로는 함수 형태로 전달.
@@ -38,24 +47,39 @@ const InfoTestUseEffect = () => {
   // 리액트 컴포넌트는 대문자 시작,
   // 기존 DOM , 소문자 태그.
   return (
-    <div>
+    <>
+      <Button
+        type="primary"
+        onClick={() => {
+          setVisible(!visible);
+          console.log(visible);
+        }}
+      >
+        {" "}
+        {visible ? "show" : "hide"}
+      </Button>
+      <div>{visible ? "true" : "false"}</div>
       <div>
-        {/* 입력창인데, 값을 입력시, onChange 이벤트 핸들러 동작해서, 결과 뷰에 반영 */}
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickName} />
+        {/* <div style={!{ visible } ? { display: "flex" } : { display: "none" }}> 원래 속성, 값이며 두번째에 있는 display 자리는 값을 넣어야하는데 계속 display를 넣어서 오류난 부분 */}
+        <div style={{ display: !visible ? "flex" : "none" }}>
+          {/* <div style={{ display: "flex" }}> */}
+          {/* 입력창인데, 값을 입력시, onChange 이벤트 핸들러 동작해서, 결과 뷰에 반영 */}
+          <input value={name} onChange={onChangeName} />
+          <input value={nickname} onChange={onChangeNickName} />
+        </div>
+        {/* 결과 뷰 출력 */}
+        <div>
+          <h1>
+            이름: <b>{name}</b>
+          </h1>
+        </div>
+        <div>
+          <h1>
+            닉네임: <b>{nickname}</b>
+          </h1>
+        </div>
       </div>
-      {/* 결과 뷰 출력 */}
-      <div>
-        <h1>
-          이름: <b>{name}</b>
-        </h1>
-      </div>
-      <div>
-        <h1>
-          닉네임: <b>{nickname}</b>
-        </h1>
-      </div>
-    </div>
+    </>
   );
 };
 
