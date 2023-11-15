cd4ec3a3123d04d0d7d510978bce7f91e78f1a35
Hyeonseok0627 <stonehead0627@gmail.com>
2023-11-15 09:09:57 +0900

    231114 7교시 axios 이용, newsapi 활용, css 배경 넣기

--- chjoin-test/src/ch13_api_PublicDataTest/component/Categories.jsx
+++ chjoin-test/src/ch13_api_PublicDataTest/component/Categories.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styled, { css } from "styled-components";
 
 // 카테고리 목록, 보이는 건 한글, 값은 영어로 할당.
@@ -17,13 +18,46 @@ const CategoriesBlockCss = styled.div`
   padding: 1rem;
   width: 768px;
   margin: 0 auto;
+  background-image: url("https://cdn.pixabay.com/animation/2022/07/30/22/01/22-01-29-66_512.gif");
+  background-size: 10%;
   @media screen and (max-width: 768px) {
     width: 100%;
     overflow-x: auto;
   }
 `;
 
-const CategoriesCss = styled.div`
+// 기존방식
+// const CategoriesCss = styled.div`
+//   font-size: 1.5rem;
+//   cursor: pointer;
+//   white-space: pre;
+//   text-decoration: none;
+//   color: inherit;
+//   padding-bottom: 0.5rem;
+
+//   &:hover {
+//     color: red;
+//   }
+
+//   // active 클래스 속성 추가.
+//   ${(props) =>
+//     props.active &&
+//     css`
+//       font-weight: 600;
+//       border-bottom: 2px solid violet;
+//       color: aqua;
+//       &:hover {
+//         color: green;
+//       }
+//     `}
+
+//   & + & {
+//     margin-left: 1rem;
+//   }
+// `;
+
+// NavLink 방식. 변경.
+const CategoriesCss = styled(NavLink)`
   font-size: 1.5rem;
   cursor: pointer;
   white-space: pre;
@@ -36,33 +70,52 @@ const CategoriesCss = styled.div`
   }
 
   // active 클래스 속성 추가.
-  ${(props) =>
-    props.active &&
-    css`
-      font-weight: 600;
-      border-bottom: 2px solid violet;
-      color: aqua;
-      &:hover {
-        color: green;
-      }
-    `}
+  // 여기 부분 변경됨, NavLink 방식으로
+  &.active {
+    font-weight: 600;
+    border-bottom: 2px solid violet;
+    color: aqua;
+    &:hover {
+      color: green;
+    }
+  }
 
   & + & {
     margin-left: 1rem;
   }
 `;
-
 {
   /* <Categories category={category} onSelect={onSelect} /> */
 }
+//기존 방식
+// const Categories = ({ category, onSelect }) => {
+//   return (
+//     <CategoriesBlockCss>
+//       {categories.map((c) => (
+//         <CategoriesCss
+//           key={c.name}
+//           active={category === c.name}
+//           onClick={() => onSelect(c.name)}
+//         >
+//           {c.text}
+//         </CategoriesCss>
+//       ))}
+//     </CategoriesBlockCss>
+//   );
+// };
+
+// NavLink 방식. 변경.
 const Categories = ({ category, onSelect }) => {
   return (
     <CategoriesBlockCss>
       {categories.map((c) => (
         <CategoriesCss
           key={c.name}
-          active={category === c.name}
-          onClick={() => onSelect(c.name)}
+          // NavLink 변경 부분
+          className={({ isActive }) => (isActive ? "active" : undefined)}
+          to={
+            c.name === "all" ? "/newsPageTest/all" : `/newsPageTest/${c.name}`
+          }
         >
           {c.text}
         </CategoriesCss>
